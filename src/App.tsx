c4cdafa030be12e687bcef7bb094ec4451e4524d
ShadyMount <shady2142@gmail.com>
2023-01-19 15:27:57 +0300

    feat: polygon added, swr added

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,36 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import "maplibre-gl/dist/maplibre-gl.css";
-import Map, { Source, Layer } from "react-map-gl";
+import Map from "react-map-gl";
 import maplibregl from "maplibre-gl";
+import useSWR from "swr";
+import Polygon, { IPolygon } from "./components/Polygon";
 
 function App() {
-  // const TestMap = new maplibregl.Map({
-  //   container: "map",
-  //   style: "https://demotiles.maplibre.org/style.json", // stylesheet location
-  //   center: [-74.5, 40], // starting position [lng, lat]
-  //   zoom: 9, // starting zoom
-  // });
-  const geojson: any = {
-    type: "FeatureCollection",
-    features: [
-      {
-        type: "Feature",
-        geometry: { type: "Point", coordinates: [-122.4, 37.8] },
-      },
-    ],
-  };
+  const fetcher = (url: RequestInfo | URL, init?: RequestInit) =>
+    fetch(url, init).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "http://agro.energomera.ru:3060/api/field?lastChangeDate=2022-01-01T10:00:00.000&skip=0&take=100",
+    fetcher
+  );
+  if (error) return <div>ошибка загрузки</div>;
+  if (isLoading) return <div>загрузка...</div>;
 
-  const layerStyle: any = {
-    id: "point",
-    type: "circle",
-    paint: {
-      "circle-radius": 10,
-      "circle-color": "#007cbf",
-    },
-  };
   return (
     <Map
       mapLib={maplibregl}
       initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14,
+        longitude: 45.3385364852744, // либо центр первого полигона
+        latitude: 42.50865653359858,
+        zoom: 12,
       }}
-      style={{ width: 600, height: 400 }}
+      style={{ width: 1200, height: 800 }}
       mapStyle={`https://api.maptiler.com/maps/hybrid/style.json?key=${
         import.meta.env.VITE_API_KEY
       }`}
     >
-      <Source type="geojson" data={geojson}>
-        <Layer {...layerStyle} />
-      </Source>
+      {data.map((e: IPolygon) => (
+        <Polygon key={e.$id} {...e} />
+      ))}
     </Map>
   );
 }
